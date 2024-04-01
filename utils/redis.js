5cc152d644dcca1f221e5e2d7510c788da8932f5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-01 20:23:06 +0000

    Add comment

--- utils/redis.js
+++ utils/redis.js
@@ -2,6 +2,7 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
+  // Builder of the class that creates a new Redis client
   constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => {
@@ -9,20 +10,24 @@ class RedisClient {
     });
   }
 
+  // Method to check if the Redis client is connected
   isAlive() {
     return this.client.connected;
   }
 
+  // Method to retrieve a value associated with a key in Redis
   async get(key) {
     const getKey = promisify(this.client.get).bind(this.client);
     const value = await getKey(key);
     return value;
   }
 
+  // Method to set a value for a key in Redis
   async set(key, value, duration) {
     this.client.set(key, value, 'EX', duration);
   }
 
+  // Method to delete a key and its associated value from Redis
   async del(key) {
     this.client.del(key);
   }
