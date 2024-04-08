8609868fcca7069c5b3c7b6892798ccd1cf18a90
Mathieu Morel <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-08 23:27:36 +0200

    Ajout du fichier de test pour DB Client

---
+++ test/dbClient.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const dbClient = require('../dbClient');
+
+__mocha().describe('DBClient Tests', () => {
+   it('Specific DBClient Test', async () => {
+        // Implement tests here
+   });
+});
