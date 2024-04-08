ae193b14a4a66c37788d3cf7019a55d427238f6e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-08 19:42:44 +0000

    Creation of worker.js to process the queue of image thumbnails with error management and creation of various sizes.

---
+++ worker.js
@@ -0,0 +1,31 @@
+import Bull from 'bull';
+import { ObjectId } from 'mongodb';
+import fs from 'fs';
+import imageThumbnail from 'image-thumbnail';
+import dbClient from './utils/db';
+
+const fileQueue = new Bull('fileQueue');
+
+// Process the fileQueue
+fileQueue.process(async (job) => {
+  const { fileId, userId } = job.data;
+
+  // Check if the fileId and userId are provided
+  if (!fileId || !userId) {
+    throw new Error('Missing fileId or userId');
+  }
+
+  // Retrieve the file from the database
+  const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  // Check if the file is an image
+  const sizes = [100, 250, 500];
+  // Generate thumbnails for each size
+  sizes.forEach(async (size) => {
+    const thumbnail = await imageThumbnail(file.localPath, { width: size });
+    fs.writeFileSync(`${file.localPath}_${size}`, thumbnail);
+  });
+});
