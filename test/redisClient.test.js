d8ace4c4b46d8bde4fc64ad9e3ca6b8af57b1c31
Mathieu Morel <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-08 23:27:24 +0200

    Ajout du fichier de test pour Redis Client

---
+++ test/redisClient.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const redisClient = require('../redisClient');
+
+__mocha().describe('RedisClient Tests', () => {
+   it('Specific RedisClient Test', async () => {
+        // Implement tests here
+   });
+});
