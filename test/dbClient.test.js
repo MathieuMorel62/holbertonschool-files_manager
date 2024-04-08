5417d6484646e3ba1a393af9ba5c011ffabd33a4
Mathieu Morel <113856302+MathieuMorel62@users.noreply.github.com>
2024-04-08 23:29:07 +0200

    Delete test directory

--- test/dbClient.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const dbClient = require('../dbClient');
-
-__mocha().describe('DBClient Tests', () => {
-   it('Specific DBClient Test', async () => {
-        // Implement tests here
-   });
-});
--- test/redisClient.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const redisClient = require('../redisClient');
-
-__mocha().describe('RedisClient Tests', () => {
-   it('Specific RedisClient Test', async () => {
-        // Implement tests here
-   });
-});
